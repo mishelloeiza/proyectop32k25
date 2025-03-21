5163b12d89238fe7a63ac01f9da968f6cd8b7271
ContrerasKathia <kcontrerasr3@miumg.edu.gt>
2025-03-21 11:09:50 -0600

    MANTENIMIENTO LMD

--- proyectoumg/Integracion/ProyectoFinal/scripts/LDD - GRUPO2 - PERFIL.sql
+++ proyectoumg/Integracion/ProyectoFinal/scripts/LDD - GRUPO2 - PERFIL.sql
@@ -5,7 +5,3 @@ CREATE TABLE perfiles(
   	PRIMARY KEY (id_perfil)
 ) ENGINE = INNODB CHARSET =latin1;
 
-INSERT INTO perfiles (`id_perfil`, `nombre_perfil`, `estatus_perfil`) VALUES
-('111','Gerentes', '1'),
-('222','Proveedores', '1'),
-('333','Inversores', '1');

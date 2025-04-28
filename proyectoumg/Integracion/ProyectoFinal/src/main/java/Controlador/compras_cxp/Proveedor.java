f34fc8c467dcc5cb89f70f2792f57621c5aace98
Oscar Morales <omoralesl7@miumg.edu.gt>
2025-04-28 11:21:25 -0600

    Agregando Java de Proveedores

---
+++ proyectoumg/Integracion/ProyectoFinal/src/main/java/Controlador/compras_cxp/Proveedor.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.compras_cxp;
+
+/**
+ *
+ * @author visitante
+ */
+public class Proveedor {
+    
+    int id_proveedor;
+    String nombre_proveedor;
+    String direccion_proveedor;
+    String telefono_proveedor;
+    String email_proveedor;
+    int saldo_proveedor;
+    int estatus_proveedor;
+    String fecha_registo; 
+
+    public Proveedor(int id_proveedor, String nombre_proveedor, String direccion_proveedor, String telefono_proveedor, String email_proveedor, int saldo_proveedor, int estatus_proveedor, String fecha_registo) {
+        this.id_proveedor = id_proveedor;
+        this.nombre_proveedor = nombre_proveedor;
+        this.direccion_proveedor = direccion_proveedor;
+        this.telefono_proveedor = telefono_proveedor;
+        this.email_proveedor = email_proveedor;
+        this.saldo_proveedor = saldo_proveedor;
+        this.estatus_proveedor = estatus_proveedor;
+        this.fecha_registo = fecha_registo;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "id_proveedor=" + id_proveedor + ", nombre_proveedor=" + nombre_proveedor + ", direccion_proveedor=" + direccion_proveedor + ", telefono_proveedor=" + telefono_proveedor + ", email_proveedor=" + email_proveedor + ", saldo_proveedor=" + saldo_proveedor + ", estatus_proveedor=" + estatus_proveedor + ", fecha_registo=" + fecha_registo + '}';
+    }
+
+    public int getId_proveedor() {
+        return id_proveedor;
+    }
+
+    public void setId_proveedor(int id_proveedor) {
+        this.id_proveedor = id_proveedor;
+    }
+
+    public String getNombre_proveedor() {
+        return nombre_proveedor;
+    }
+
+    public void setNombre_proveedor(String nombre_proveedor) {
+        this.nombre_proveedor = nombre_proveedor;
+    }
+
+    public String getDireccion_proveedor() {
+        return direccion_proveedor;
+    }
+
+    public void setDireccion_proveedor(String direccion_proveedor) {
+        this.direccion_proveedor = direccion_proveedor;
+    }
+
+    public String getTelefono_proveedor() {
+        return telefono_proveedor;
+    }
+
+    public void setTelefono_proveedor(String telefono_proveedor) {
+        this.telefono_proveedor = telefono_proveedor;
+    }
+
+    public String getEmail_proveedor() {
+        return email_proveedor;
+    }
+
+    public void setEmail_proveedor(String email_proveedor) {
+        this.email_proveedor = email_proveedor;
+    }
+
+    public int getSaldo_proveedor() {
+        return saldo_proveedor;
+    }
+
+    public void setSaldo_proveedor(int saldo_proveedor) {
+        this.saldo_proveedor = saldo_proveedor;
+    }
+
+    public int getEstatus_proveedor() {
+        return estatus_proveedor;
+    }
+
+    public void setEstatus_proveedor(int estatus_proveedor) {
+        this.estatus_proveedor = estatus_proveedor;
+    }
+
+    public String getFecha_registo() {
+        return fecha_registo;
+    }
+
+    public void setFecha_registo(String fecha_registo) {
+        this.fecha_registo = fecha_registo;
+    }
+      
+    public Proveedor() {
+    }
+   
+}

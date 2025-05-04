ed7f858b9da2431b0befb2eb908b4c9535c80ec7
ContrerasKathia <kcontrerasr3@miumg.edu.gt>
2025-05-03 22:52:58 -0600

    Subiendo java de metodo de pago

---
+++ proyectoumg/Integracion/ProyectoFinal/src/main/java/Controlador/compras_cxp/Metododepago.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador.compras_cxp;
+
+/**
+ *
+ * @author visitante
+ */
+public class Metododepago {
+    int id_metodoPago;
+    String nombreMetodoPago;
+    String estatusMetodoPago;
+
+    public Metododepago(int id_metodoPago, String nombreMetodoPago, String estatusMetodoPago) {
+        this.id_metodoPago = id_metodoPago;
+        this.nombreMetodoPago = nombreMetodoPago;
+        this.estatusMetodoPago = estatusMetodoPago;
+    }
+
+    @Override
+    public String toString() {
+        return "Metododepago{" + "id_metodoPago=" + id_metodoPago + ", nombreMetodoPago=" + nombreMetodoPago + ", estatusMetodoPago=" + estatusMetodoPago + '}';
+    }
+
+    public void setId_metodoPago(int id_metodoPago) {
+        this.id_metodoPago = id_metodoPago;
+    }
+
+    public void setNombreMetodoPago(String nombreMetodoPago) {
+        this.nombreMetodoPago = nombreMetodoPago;
+    }
+
+    public void setEstatusMetodoPago(String estatusMetodoPago) {
+        this.estatusMetodoPago = estatusMetodoPago;
+    }
+
+    public int getId_metodoPago() {
+        return id_metodoPago;
+    }
+
+    public String getNombreMetodoPago() {
+        return nombreMetodoPago;
+    }
+
+    public String getEstatusMetodoPago() {
+        return estatusMetodoPago;
+    }
+
+    public Metododepago() {
+    }
+
+    
+}
